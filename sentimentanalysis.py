8d4e86032a32c43a436ce6ade1f9f9a5995fe259
ddevlin678 <48292311+ddevlin678@users.noreply.github.com>
2019-05-09 00:01:12 +0100

    Update and rename needscomments.py to sentimentanalysis.py

--- sentimentanalysis.py
+++ sentimentanalysis.py
@@ -38,18 +38,53 @@ pd.set_option('display.max_colwidth', -1)
 pd.set_option('display.expand_frame_repr', False)
 #%matplotlib.pyplot as plt
 data = []
+#counters 
 TeamAPos = int()
 TeamBPos = int()
 TeamANeut = int()
 TeamBNeut = int()
 TeamANeg = int()
 TeamBNeg = int()
-
 pos = int() 
 neg = int()
 neut = int()
 data = []
-start = datetime.datetime(2019, 3, 17, 16, 30, 00)
+#determining tweets posted during live-play
+start1 = datetime.datetime(2019, 3, 17, 16, 30, 00) #cheeve
+start2 = datetime.datetime(2019, 4, 22, 20, 00, 00) #chebur 
+start3 = datetime.datetime(2019, 4, 21, 13, 30, 00) #evemun
+start = datetime.datetime(2019, 4, 20, 12, 31, 00) #mcitot
+
+
+#list of alias' 
+PremierLeague = {}
+PremierLeague['Arsenal'] = ["Arsenal", "The Gunners", "ARS", "Gunners"]
+PremierLeague['Aston Villa'] = [ "Aston Villa", "The Villains", "Villa", "AST"]
+PremierLeague['Burnley'] = [ "The Clarets", "Burnley"]
+PremierLeague['Chelsea'] = ["Chelsea", "The Blues", "The Pensioners", "CHE", "CFC"]
+PremierLeague['Crystal Palace'] = ["Crystal Palace","Crystal", "Palace", "The Eagles", "CRY"]
+PremierLeague['Everton'] =[ "Everton", "The Toffees", "EVE"]
+PremierLeague['Hull City'] = ["Hull City", "Hull", "The Tigers"]
+PremierLeague['Leicester City'] = ["Leicester City", "Leicester", "The Foxes", "LEI"]
+PremierLeague['Liverpool'] = ["Liverpool", "The Reds", "LIV", "YNWA", "Reds", "LFC", "LFCFamily"]
+PremierLeague['Manchester City'] = ["Manchester City", "City", "Cityzens", "MCI", "CTID" ,"MCFC"]
+PremierLeague['Manchester United'] = ["Manchester United", "United", "The Red Devils", "MUN"]
+PremierLeague['Newcastle United'] = ["Newcastle United", "Newcastle", "The Magpies", "NEW"]
+PremierLeague['Queens Park Rangers'] = ["Queens Park Rangers", "Super Hoops", "QPR"]
+PremierLeague['Southampton'] = ["Southampton", "The Saints", "SOU"]
+PremierLeague['Stoke City'] = ["Stoke City", "The Potters", "STK"]
+PremierLeague['Sunderland'] = ["Sunderland", "The Black Cats", "SUN"]
+PremierLeague['Swansea City'] = ["Swansea City", "Swansea", "The Swans", "SWA"]
+PremierLeague['Tottenham Hotspur'] = ["Tottenham Hotspur", "Tottenham", "The Spurs", "TOT"]
+PremierLeague['Watford'] = ["Watford", "Hornets", "Yellow Army", "WFC"]
+PremierLeague['West Bromwich Albion'] = ["West Bromwich" "Albion", "West Bromwich", "The Baggies", "WBA"]
+PremierLeague['West Ham United'] = ["West Ham United", "West Ham",  "The Hammers", "WHU"]
+PremierLeague['FC Schalke 04'] = ["FC Schalke 04", "Schalke", "The Royal Blues", "S04"]
+PremierLeague['Atletico Madrid'] = ["Atletico Madrid", "ATM"]
+PremierLeague['Juventus'] = ["Juventus", "JUVE" ,"JUV"]
+PremierLeague['Bayern Munich'] = ["Bayern Munich", "Bayern" ,"Munich", "FCB"]
+PremierLeague['Dynamo Kyiv'] = ["dynamo kyiv", "FC Dynamo Kyiv", "DYN"]
+
 
 incEvents = {}
 wordCloudNegB = []
@@ -57,9 +92,8 @@ wordCloudPosA = []
 wordCloudPosB = []
 wordCloudNegA = []
 listofDict = []
-
-
-dictionary = {
+#pick game to analysis or create new dictionary if new dataset
+dictionary1 = {
     "TeamA": ["Chelsea", "Blues","Pensioners", "CHE", "CFC", "chelseafc"],
     "TeamB": ["Everton", "Toffees",  "EVE", "EFC"]
     }
@@ -68,13 +102,14 @@ dictionary2 = {
     "TeamB": ["Clarets", "Burnley",  "BFC"]
     }
 dictionary3 = {
-    "TeamA": ["ManU", "United","Manchester", "MUFC", "Reds", ],
-    "TeamB": ["Everton", "Toffees",  "EVE", "EFC"]
+    "TeamA": ["Everton", "Toffees",  "EVE", "EFC"],
+    "TeamB": ["ManU", "United","Manchester", "MUFC", "Reds"]
     }
-dictionary4 = {
-    "TeamA": ["Arsenal", "Gunners","ARS", "AFC", "COYG"],
-    "TeamB": ["Crystal", "Palace",  "Eagles", "CRY"]
+dictionary = {
+    "TeamA": ["Manchester City", "Cityzens","MCI", "CTID", "MCFC"],
+    "TeamB": ["Tottenham Hotspur", "Tottenham",  "Spurs", "TOT"]
     }
+#producing word clouds for tweets featured within positive and negative tweets for each team
 def wordCloud(wordcloud, title = None):
  stopwords = set(STOPWORDS)
  i = -1
@@ -88,7 +123,7 @@ def wordCloud(wordcloud, title = None):
         max_words=100,
         max_font_size=40, 
         scale=3,
-        random_state=1 # chosen at random by flipping a coin; it was heads
+        random_state=1
     ).generate(str(new))
   fig = plt.figure(1, figsize=(12, 12))
   fig.canvas.set_window_title(titleList[i])
@@ -96,7 +131,7 @@ def wordCloud(wordcloud, title = None):
   plt.imshow(wordcloud)
   plt.title(titleList[i])
   plt.show()
-
+#providing sentiment weight and linking tweets to teams and creating word banks for word clouds
 def sentiment(row, live):
  tweetObject = row[1]
  tweetObject = str(tweetObject)
@@ -158,12 +193,10 @@ def sentiment(row, live):
       TeamBNeg +=1
    global neg
    neg = neg + 1
-
- 
- try:
+ try: 
    return (lb,teamType)
- except ZeroDivisionError:
-  print("")
+ except:
+  print("Error")
 
 def eventTime(row): # want to increment a count of the occurrence of any of the events types
  events = {}
@@ -181,12 +214,18 @@ def eventTime(row): # want to increment a count of the occurrence of any of the 
 
  for event in events:
   for h in events[event]:
-   tweetObject = row[1]#.values
+   tweetObject = row[1]
    tweetObject = str(tweetObject)
    if re.search(h, tweetObject, re.IGNORECASE):
     incEvents[event] +=1
+ 
+  
+  #eveche2019-03-17-rt1.csv
+  #chebur134.csv
+  #mcitot134.csv
+  #evemun134.csv
 
-with open('eveche2019-03-17-rt1.csv', 'r') as csvfile:
+with open('mcitot134.csv', 'r') as csvfile:
  readCSV = csv.reader(csvfile, delimiter=',')
  for row in readCSV:
   data.append(row)
@@ -219,10 +258,10 @@ with open('eveche2019-03-17-rt1.csv', 'r') as csvfile:
   aResult = 0
   bResult = 0
   checkLive = False
+
   for x in group[1:]:
    for row in x.values:
     tweetTime = row[2]
-
     tweetTime = datetime.datetime.strptime(tweetTime, '%Y-%m-%d  %H:%M:%S')
     firsthalf = start + timedelta(minutes=48)
     secondhalf = firsthalf + timedelta(minutes=15)
@@ -237,32 +276,24 @@ with open('eveche2019-03-17-rt1.csv', 'r') as csvfile:
     if("A" in teamType):
      teamATotalSentiment += tweetSentiment
      teamATotalTweets += 1
-     #print(teamATotalSentiment, teamATotalTweets)
    
     if("B" in teamType):
      teamBTotalSentiment += tweetSentiment
      teamBTotalTweets += 1
-     #print(teamBTotalSentiment, teamBTotalTweets)
 
-   #totalSentiment += tweetSentiment
     if (live):
      checkLive = True
      event = eventTime(row)
-
      tweetTuple = sentiment(row,live)
      tweetSentiment = (tweetTuple[0])
-     teamType = tweetTuple[1]              
+     teamType = tweetTuple[1]  
      if ("A" in teamType):
       teamAMinuteSentiment += tweetSentiment
       teamAMinuteTweets += 1
-      
    
      if ("B" in teamType):
       teamBMinuteSentiment += tweetSentiment
       teamBMinuteTweets += 1
-  
-
-   #return after each minute is that still in scope
 
      if teamAMinuteTweets != 0:
       aResult = (teamAMinuteSentiment/teamAMinuteTweets)
@@ -273,7 +304,7 @@ with open('eveche2019-03-17-rt1.csv', 'r') as csvfile:
    myListB.append(bResult)
    listofDict.append(incEvents)
    checkLive = False
- 
+
  wordcloud = []
  flat_list1 = [item for sublist in wordCloudPosA for item in sublist]
  flat_list2 = [item for sublist in wordCloudPosB for item in sublist]
@@ -284,8 +315,7 @@ with open('eveche2019-03-17-rt1.csv', 'r') as csvfile:
  wordcloud.append(nltk.FreqDist(flat_list3).most_common(150))
  wordcloud.append(nltk.FreqDist(flat_list4).most_common(150))
  wordCloud(wordcloud)
-
-
+ 
  def plotting(myListA,myListB):
   y1= myListA[:96]
   y2= myListB[:96]
@@ -296,6 +326,7 @@ with open('eveche2019-03-17-rt1.csv', 'r') as csvfile:
   plt.plot(x, y1, '-b', label='Chelsea')
   plt.plot(x2, y2, '-r', label='Everton')
   plt.legend(loc='upper left')
+ 
   plt.title('Sentiment throughout game')
   plt.show()
 
@@ -305,21 +336,22 @@ with open('eveche2019-03-17-rt1.csv', 'r') as csvfile:
   labels = ['TeamAPos', 'TeamANeut', 'TeamANeg', 'TeamBPos', 'TeamBNeut', 'TeamBNeg' ] 
   plt.pie(values, labels=labels, autopct='%.2f')
   plt.show()
-
-
  plotting(myListA,myListB)
  pie(TeamAPos,TeamBPos,TeamANeut,TeamBNeut,TeamANeg,TeamBNeg)
 
-
+ 
+ eventList2 = []
  dictkeys = ["goal","redCard","penalty","substitution","injury","freeKick","offside","assist","yellowcard"]
+
  for event in dictkeys:
   eventList = []
-  for i in range(0,96):#len(listofDict):
+  for i in range(0,96):
    eventList.append(listofDict[i][event])
   sd= statistics.stdev(eventList)
   median=statistics.median(eventList)
   mad = median_absolute_deviation(eventList)
   print("event",event,"sd",sd,"median",median,"mad",mad)
   for i in range(0,96):
-   if eventList[i] > mad*10:#or median*1000
-    print(str(event), "may of happened at minute", i) #row.index(x)
+   
+   if eventList[i] > sd*3.6:
+    print(str(event), "may of happened at minute", i)
